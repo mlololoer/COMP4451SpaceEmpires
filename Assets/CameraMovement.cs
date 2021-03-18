88b28fc34f2464f93ec977d36c21d0b9a290182b
Dhulan Seniru Daluwatte <dsdaluwatte@connect.ust.hk>
2021-03-18 16:04:38 +0800

    comment
    concerning cuteness

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -7,7 +7,7 @@ public class CameraMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    //pedro looks cute today 
     }
 
     // Update is called once per frame
